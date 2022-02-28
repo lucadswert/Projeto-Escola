8239878d32ea1732bab44c97bc8fe75b6c510697
Jeferson Lors <69722947+JefersonLors@users.noreply.github.com>
2022-02-28 16:49:27 +0000

    Incremento nos menus e configuração de loopin entre os menus.

--- Projeto.c
+++ Projeto.c
@@ -7,7 +7,6 @@
 #define true 1
 #define false 0
 
-
 //Dados - Alunos e Professores//
 /// Formato CPF: 0 0 0 . 0 0 0 . 0 0 0 - 0 0 '\0' \n
 ///              0 1 2 3 4 5 6 7 8 9 0 1 2 3   4   5 
@@ -60,41 +59,67 @@ int main( ){
     void cadastrar( ); 
     void listar( );
     
-	int resposta;
+	char resposta = '\0';
 
-    enum MENU{ Realizar_Cadastro = 1, Listar_Informações = 2, Encerrar_Programa = 0};
-    
-    printf ("=====================================\n"
-            "            Projeto Escola           \n"
-            "=====================================\n\n");	
-    do{
-        printf ("MENU \n"
-            "[1] | Realizar Cadastro\n"
-            "[2] | Listar Informações\n"
-            "[0] | Encerrar Programa\n\n-> ");
-        scanf("%d", &resposta);
-        if(!(resposta == Realizar_Cadastro) && !(resposta == Listar_Informações) &&             !(resposta == Encerrar_Programa) ){
-            puts("  < Valor inserido inválido! >\n");}
-    }while( !(resposta == Realizar_Cadastro) && !(resposta == Listar_Informações) &&             !(resposta == Encerrar_Programa) );
-    
-    switch( resposta ){
-        case Realizar_Cadastro : cadastrar( ); break;
-        case Listar_Informações: listar( ); break;
-        case Encerrar_Programa: printf ("Programa Encerrado\n"); break;}
+    enum MENU{ Realizar_Cadastro = '1', Listar_Informações = '2', Encerrar_Programa = '0'};
+
+    do{ 	
+        do{
+            puts( "=====================================\n"
+                  "            Projeto Escola           \n"
+                  "=====================================\n");
+            printf ("MENU PRINCIPAL\n"
+                    "[1] | Realizar Cadastro\n"
+                    "[2] | Listar Informações\n"
+                    "[0] | Encerrar Programa\n\n-> ");
+            resposta = getchar( ); getchar( ); 
+            putchar('\n');
+            if( !(resposta == Realizar_Cadastro) && !(resposta == Listar_Informações) && !(resposta == Encerrar_Programa) ){
+                puts("  < Valor inserido inválido! >\n");}
+        }while( !(resposta == Realizar_Cadastro) && !(resposta == Listar_Informações) &&  !(resposta == Encerrar_Programa) );
+        
+        switch( resposta ){
+            case Realizar_Cadastro : cadastrar( ); break;
+            case Listar_Informações: listar( ); break;
+            case Encerrar_Programa: printf ("Programa Encerrado\n"); break;}
+    }while( resposta != Encerrar_Programa );
     return EXIT_SUCCESS;
 }
 void cadastrar( ){
-    printf ("Insira um número: \n");	
-    printf ("[1] | Cadastrar Alunos\n");
-    printf ("[2] | Cadastrar Disciplinas\n");	
-    //scanf("%d", &valor2);
-    
+    char resposta = '\0';
+    enum MENU { Professor = '1', Aluno = '2', Disciplina = '3', Voltar = '0'};
+
+    do{
+        puts( "=====================================\n"
+              "             MENU CADASTRO           \n"
+              "=====================================\n");	
+        printf( "CADASTRAR: \n"	
+                "[1] | Aluno\n"
+                "[2] | Professor\n"
+                "[3] | Disciplina\n"
+                "[0] | Voltar\n\n-> ");
+        resposta = getchar( ); getchar( );
+        putchar('\n');
+        if( !(resposta == Professor) && !(resposta == Aluno) && !(resposta == Disciplina) && !(resposta == Voltar) ){
+            puts( "  < Valor inserido inválido! >\n" );}
+    }while( !(resposta == Professor) && !(resposta == Aluno) && !(resposta == Disciplina) && !(resposta == Voltar) );
 }
 void listar( ){
-    printf ("Insira um número: \n");	
-    printf ("[1] | Listar Alunos\n");
-    printf ("[2] | Listar Professores\n");
-    printf ("[3] | Listar Disciplinas\n");
-    //scanf("%d", &valor2);  
-    
+    char resposta = '\0';
+    enum MENU { Professor = '1', Aluno = '2', Disciplina = '3', Voltar = '0' };
+
+    do{
+        puts("=====================================\n"
+             "             MENU LISTAS             \n"
+             "=====================================\n");	
+        printf( "LISTAR: \n"	
+                "[1] | Aluno\n"
+                "[2] | Professor\n"
+                "[3] | Disciplina\n"	
+                "[0] | Voltar\n\n-> " );
+        resposta = getchar( ); getchar( ); 
+        putchar('\n');
+        if( !(resposta == Professor) && !(resposta == Aluno) && !(resposta == Disciplina) && !(resposta == Voltar)){
+            puts( "  < Valor inserido inválido! >\n" );}
+    }while( !(resposta == Professor) && !(resposta == Aluno) && !(resposta == Disciplina) && !(resposta == Voltar) );
 };
