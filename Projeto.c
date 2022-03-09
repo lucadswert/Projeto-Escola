713d85e98acf1ed79952d0dfc1f9e34c32df4098
Lucas Figueredo Ribeiro <75700518+lucadswert@users.noreply.github.com>
2022-03-09 21:30:39 +0000

    Implementação solicita em todas as categorias (falta validação)

--- Projeto.c
+++ Projeto.c
@@ -58,11 +58,21 @@ int professoresCadastrados = 0,
     alunosCadastrados = 0,
     disciplinasCadastradas = 0;
 
-char *validaMatricula( char *matricula );
-char *validaCpf( char *cpf );
-char *validaSexo( char sexo );
-char *validaNascimento( char *data );
-char *validaNome( char *nome );
+//Validação de dados de professores e alunos
+bool validaMatricula( char *matricula );
+bool validaCpf( char *cpf );
+bool validaSexo( char sexo );
+bool validaNascimento( char *data );
+bool validaNome( char *nome );
+
+///validação de dados de matricula
+bool validaNomeDiscplina( char* );
+bool validaCodigo( char* );
+bool validaSemestre( char* );
+bool validaVagas( int );
+bool validaQuanMatr( int );
+bool validaProfessores( char* );
+
 ///Adicionar função de limpeza de buffer para caso o usuário insira uma quantidade de dados maior do que ///a solicitada
 
 int main( ){
@@ -97,6 +107,7 @@ int main( ){
 
     return EXIT_SUCCESS;
 }
+
 void cadastrar( ){
     int menuCadastrar( );
     bool cadastrarAluno( );
@@ -112,7 +123,7 @@ void cadastrar( ){
                 if( !cadastrarProfessor( ) ){
                     puts( "\n\tOps!Professor não cadastrado! ");}
                 break;
-           // case Aluno: cadastrarAluno(); break;
+            case Aluno: cadastrarAluno(); break;
             //case Disciplina: cadastrarDisciplina(); break;
             //case Voltar: break;     
         }
@@ -158,6 +169,7 @@ void listar( ){
     
     //puts( "\t>>>> OPÇÃO EM MANUTENÇÃO <<<<\n" );
 }
+
 int menuCadastrar( ){
     char resposta = '\0';
     enum MENU { Aluno = '1', Professor = '2', Disciplina = '3', Voltar = '0'};
@@ -179,6 +191,7 @@ int menuCadastrar( ){
     
     return resposta;
 }
+
 int menuListar( ){
     char resposta = '\0';
     enum MENU { Aluno = '1', Professor = '2', Disciplina = '3', Voltar = '0'};
@@ -201,6 +214,7 @@ int menuListar( ){
     return resposta;
     
 }
+
 bool cadastrarProfessor( ){
     bool solicitaMatriculaProfessor( );
     bool solicitaNomeProfessor( );
@@ -222,136 +236,321 @@ bool cadastrarProfessor( ){
 }
 
 bool solicitaMatriculaProfessor( ){ ////REPLICAR ESSE MODELO NAS DEMAIS FUNÇÕES
-    char *mensagemDeErro = NULL;
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
     static int posicao = 0;
     
     do{
         printf("Digite o numero de matricula: \n");
         fgets((docente[posicao].dado.matricula), TAM_MAT, stdin);
         ///criar função que tirar o \n do final da string e inserir ela aqui tb
-        mensagemDeErro = validaMatricula( docente[posicao].dado.matricula );
-        if( mensagemDeErro != NULL ){
-            puts( mensagemDeErro ); }
-    }while( mensagemDeErro != NULL );
+        DADO = validaMatricula( docente[posicao].dado.matricula );
+    }while( DADO == INVALIDO );
     
     posicao++;
     return true;
 }
-char *validaMatricula( char *matricula ){
-  //FaltaFazer//
-    return NULL;
-}
-
 bool solicitaNomeProfessor( ){
-    char *mensagemDeErro = NULL;
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
     static int posicao = 0;
     do{
         printf("Digite o nome do professor:\n");
         fgets(docente[posicao].dado.nome, TAM_NOME, stdin);
-        mensagemDeErro = validaNome (docente[posicao].dado.nome );
-        if( mensagemDeErro != NULL ){
-            puts (mensagemDeErro); }
-   }while( mensagemDeErro != NULL );
+        DADO = validaNome (docente[posicao].dado.nome );
+   }while( DADO == INVALIDO );
   
     posicao++;
     return true;
 }
-char *validaNome (char *nome){
-    //FaltaFazer//
-    return NULL;
-}
-
 bool solicitaNascimentoProfessor( ){
-    char *mensagemDeErro = NULL;
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
     static int posicao = 0;
+    
     do{
-    printf("Digite a data de nascimento:\n");
-    fgets(docente[posicao].dado.nascimento, TAM_NAC, stdin);
-    mensagemDeErro = validaNascimento ( docente[posicao].dado.nascimento );
-    if( mensagemDeErro != NULL ){
-            puts (mensagemDeErro); }
-   }while( mensagemDeErro != NULL );
+        printf("Digite a data de nascimento:\n");
+        fgets(docente[posicao].dado.nascimento, TAM_NAC, stdin);
+        DADO = validaNascimento ( docente[posicao].dado.nascimento );
+   }while( DADO == INVALIDO );
   
     posicao++;
     return true;
 }
-
-char *validaNascimento (char *data){
-    //FaltaFazer//
-    return NULL;
-}
-
-
 bool solicitaCpfProfessor(){
-    char *mensagemDeErro = NULL;
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
     static int posicao = 0;
+    
     do{
-    printf("Digite o CPF:\n");
-    fgets(docente[posicao].dado.cpf, TAM_CPF, stdin);
-    mensagemDeErro = validaCpf ( docente[posicao].dado.cpf );
-    if( mensagemDeErro != NULL ){
-        puts (mensagemDeErro); }
-   }while( mensagemDeErro != NULL );
+        printf("Digite o CPF:\n");
+        fgets(docente[posicao].dado.cpf, TAM_CPF, stdin);
+        DADO = validaCpf ( docente[posicao].dado.cpf );
+   }while( DADO == INVALIDO );
 
     posicao++;  
     return true;
 }
-char *validaCpf (char *cpf){
-    //FaltaFazer//
-    return NULL;
-}
-
-  bool solicitaSexoProfessor( ){
-    char *mensagemDeErro = NULL;
+bool solicitaSexoProfessor( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
     static int posicao = 0;
+    
     do{
-    printf("Digite o sexo:\n");
-    scanf("%c", &docente[posicao].dado.sexo);
-    getchar( );
-    mensagemDeErro = validaSexo (docente[posicao].dado.sexo);
-    if( mensagemDeErro != NULL ){
-        puts (mensagemDeErro); }
-   }while( mensagemDeErro != NULL );
+        printf("Digite o sexo:\n");
+        scanf("%c", &docente[posicao].dado.sexo);
+        getchar( );
+        DADO = validaSexo (docente[posicao].dado.sexo);
+    }while( DADO == INVALIDO );
 
     posicao++; 
     return true;
 }
-char *validaSexo( char sexo ){
+
+bool cadastrarAluno(){
+    bool solicitaMatriculaAluno( );
+    bool solicitaNomeAluno( );
+    bool solicitaNascimentoAluno( );
+    bool solicitaCpfAluno( );
+    bool solicitaSexoAluno( );
+
+    if( (solicitaMatriculaAluno( )) == true ){
+        if( solicitaNomeAluno( ) == true) {
+            if( solicitaNascimentoAluno( ) == true){
+                if( solicitaCpfAluno( ) == true){
+                    if( solicitaSexoAluno( ) == true){    
+                        return true;
+                    }else{ return false;}
+                }else{ return false; }
+            }else{ return false; }
+        }else{ return false;}
+    }else{ return false;}
+}  
+
+bool solicitaMatriculaAluno( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite a matricula do Aluno:\n");
+        fgets(discente[posicao].dado.matricula, TAM_MAT, stdin);
+        DADO = validaNome (discente[posicao].dado.matricula );
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+}
+bool solicitaNomeAluno(){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o nome do Aluno:\n");
+        fgets(discente[posicao].dado.nome, TAM_NOME, stdin);
+        DADO = validaNome (discente[posicao].dado.nome );
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+} 
+bool solicitaSexoAluno( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o sexo:\n");
+        scanf("%c", &discente[posicao].dado.sexo);
+        getchar( );
+        DADO = validaSexo (discente[posicao].dado.sexo);
+   }while( DADO == INVALIDO );
+
+    posicao++; 
+    return true;
+}
+bool solicitaCpfAluno( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o CPF:\n");
+        fgets(discente[posicao].dado.cpf, TAM_CPF, stdin);
+        DADO = validaCpf ( discente[posicao].dado.cpf );
+   }while( DADO == INVALIDO );
+
+    posicao++;  
+    return true;
+    
+  }
+bool solicitaNascimentoAluno( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite a data de nascimento:\n");
+        fgets(discente[posicao].dado.nascimento, TAM_NAC, stdin);
+        DADO = validaNascimento ( discente[posicao].dado.nascimento );
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+}
+
+bool validaMatricula( char *matricula ){
+  //FaltaFazer//
+    return NULL;
+}
+bool validaNome (char *nome){
     //FaltaFazer//
     return NULL;
-  }
-bool cadastrarAluno(){
-                 
-    printf("Digite o nome do aluno: ");
-    fgets(discente[0].dado.nome, TAM_NOME, stdin);
-
-    printf("Digite o sexo: ");
-    scanf("%c", &discente[0].dado.sexo);
-    getchar( );
-
-    printf("Digite o CPF: ");
-    fgets(discente[0].dado.cpf, TAM_CPF, stdin);
-
-    printf("Digite a data de nascimento: ");
-    fgets(discente[0].dado.nascimento, TAM_NAC, stdin);
-    return true;
 }
+bool validaNascimento (char *data){
+    //FaltaFazer//
+    return NULL;
+}
+bool validaCpf (char *cpf){
+    //FaltaFazer//
+    return NULL;
+}
+bool validaSexo( char sexo ){
+    //FaltaFazer//
+    return NULL;
+}
+
 bool cadastrarDisciplina(){
-    printf("Digite o nome da disciplina: ");
-    fgets(materia[0].nome, TAM_NOME_DISC, stdin);
-               
-    printf("Digite o código da matéria: ");
-    fgets(materia[0].codigo, TAM_COD_DISC, stdin);
+    bool solicitaNomeDisciplina( );
+    bool solicitaCodigo( );
+    bool solicitaSemestre( );
+    bool solicitaVagas( );
+    bool solicitaQuanMatr( );
+    bool solicitaProfessor( );
+    
+    if( solicitaNomeDisciplina( ) ){
+        if( solicitaCodigo( ) ){
+            if( solicitaSemestre( ) ){
+                if( solicitaVagas( ) ){
+                    if( solicitaQuanMatr( ) ){
+                        if( solicitaProfessor( ) ){
+                            return true;
+                        }else{ return false;}
+                    }else{ return false;}
+                }else{ return false;}
+            }else{ return false;}
+        }else{ return false;}
+    }else{ return false;}
+}
 
-    printf("Digite a quantidade de vagas: ");
-    scanf("%d", &materia[0].vagas);
-
-    printf("Digite o semestre: ");
-    fgets(materia[0].semestre, TAM_SEM, stdin);
-
-    printf("Digite o professor da matéria: ");
-    fgets(materia[0].professor.dado.nome, TAM_NOME, stdin);
+bool solicitaNomeDisciplina( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o nome da disciplina:\n");
+        fgets(materia[posicao].nome, TAM_NOME_DISC, stdin);
+        DADO = validaNomeDiscplina (materia[posicao].nome );
+   }while( DADO == INVALIDO );
   
+    posicao++;
+    return true;
+}
+bool solicitaCodigo( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o código da disciplina:\n");
+        fgets(materia[posicao].codigo, TAM_COD_DISC, stdin);
+        DADO = validaCodigo (materia[posicao].codigo );
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+}
+bool solicitaSemestre( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o semestre da disciplina:\n");
+        fgets(materia[posicao].semestre, TAM_SEM, stdin);
+        DADO = validaSemestre (materia[posicao].semestre);
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+}
+bool solicitaVagas( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o número de vagas da disciplina:\n");
+        scanf("%d", &materia[posicao].vagas);
+        //testar com getchar dps
+        DADO = validaVagas (materia[posicao].vagas);
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+}
+bool solicitaQuanMatr( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite a quantidade de matriculados da disciplina:\n");
+        scanf( "%d", &materia[posicao].quantidadeMatriculado);
+        DADO = validaQuanMatr(materia[posicao].quantidadeMatriculado);
+   }while( DADO == INVALIDO );
+  
+    posicao++;
+    return true;
+}
+bool solicitaProfessor( ){
+    enum VALIDAR { VALIDO = 1, INVALIDO = 0 };
+    enum VALIDAR DADO;
+    static int posicao = 0;
+    
+    do{
+        printf("Digite o professor da disciplina:\n");
+        fgets(materia[posicao].professor.dado.nome, TAM_NOME, stdin);
+        DADO = validaProfessores(materia[posicao].professor.dado.nome);
+   }while( DADO == INVALIDO );
+  
+    posicao++;
     return true;
 }
 
+bool validaNomeDiscplina( char *nome ){
+    
+    return true;
+}
+bool validaCodigo( char *codigo ){
+
+    return true;
+}
+bool validaSemestre( char *semestre ){
+
+    return true;
+}
+bool validaVagas( int vagas ){
+
+    return true;
+}
+bool validaQuanMatr( int quantidadeMatriculados ){
+
+    return true;
+}
+bool validaProfessores( char *Professor ){
+
+    return true;
+}
